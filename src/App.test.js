60d10e687f9faf14d6a21f6e3c1f3a3b1579a148
Thanh Truong <thanhtruong@Thanhs-MacBook-Pro.local>
2020-01-24 19:16:42 +0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// it("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it("renders learn react link", () => {
+  const a = 1;
+  expect(a).toBe(1);
 });
+
+// it("renders learn react link", () => {
+//   const a = 1;
+//   expect(a).toBe(1);
+// });
